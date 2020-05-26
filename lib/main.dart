60afcc20f1211057fe0c388656671dbfe9bfa1c2
Paulo Borges <pborges086@hotmail.com>
2020-05-26 20:58:33 -0300

    Adicionando SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_color_plugin/flutter_color_plugin.dart';
 import 'package:nubank_flutter/pages/home_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    delay();
+  }
+
+  Future<void> delay() async {
+    return await Future.delayed(
+      Duration(seconds: 5),
+      () {
+        Navigator.pushReplacement(
+          context,
+          PageRouteBuilder(pageBuilder: (BuildContext context,
+              Animation animation, Animation secondaryAnimation) {
+            return Home();
+          }),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return Scaffold(
+      backgroundColor: ColorUtil.color("9F0899"),
+      body: Center(
+        child: Image.asset(
+          "build/images/logo_nubank.png",
+          height: 90,
+          width: 90,
+        ),
       ),
-      home: Home(),
     );
   }
 }
